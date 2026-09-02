c2be91fdbea2fa6b7fa1798df25d10a0d832549f
mgm404 <mariana.gomez.monge@gmail.com>
2026-09-02 12:33:45 -0600

    primera opcion de menu completa

--- Python Básico/Sistema de Control de Estudiantes/actions.py
+++ Python Básico/Sistema de Control de Estudiantes/actions.py
@@ -20,6 +20,53 @@ def input_students():
     student_info(amount_of_students) #Va a guardar info
 
 def student_info(amount_of_students):
-    #blablablablabla
-    for i in amount_of_students:
-        i #TODO Codigo al chile
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("++++++++++++++++++++++++++++++++++++++++++++++ \n+++++++++++ Control de estudiantes +++++++++++ \n++++++++++++++++++++++++++++++++++++++++++++++ \n++++++++++                          ++++++++++ \n+++         Ingreso de información         +++")
+
+    students=[]
+
+    for i in range(amount_of_students):
+        one_student={}
+        print(f"\n+++ Estudiante #{i+1}")
+        one_student['name']=input("+ Nombre y apellidos: ")
+        one_student['student_class']=input("+ Sección (ej. 11B):")
+
+        print("+")
+
+        spanish_grade=student_grades(spanish_grade, "Español")
+        english_grade=student_grades(english_grade, "Ingles")
+        social_studies_grade=student_grades(social_studies_grade, "Estudios sociales")
+        science_grade=student_grades(science_grade, "Ciencias")
+
+        general_average=spanish_grade+english_grade+social_studies_grade+science_grade/4
+        one_student['spanish_grade']=spanish_grade
+        one_student['english_grade']=english_grade
+        one_student['social_studies_grade']=social_studies_grade
+        one_student['science_grade']=science_grade
+        one_student['general_average']=general_average
+
+        students.append(one_student)
+
+    data.save_student_data(students)
+    print("\n+ Se guardo la información de los estudiantes \n	       correctamente!")
+    time.sleep(5)
+    go_back_menu()
+
+
+def student_grades(grade, class_of_grade):
+    while grade<0 or grade>100:
+        try:
+            grade=int(input(f"\n+ Nota total de {class_of_grade}:"))
+
+            if grade<0 or grade>100:
+                raise ValueError()
+        except ValueError:
+            print("+++++++++++++++++++ ERROR ++++++++++++++++++++ \n+    El valor ingresado tiene que ser un     + \n+	   número entre 0 y 100              + \n+            Ingreso: {grade}                + \n+        Ingrese el valor correcto           + \n++++++++++++++++++++++++++++++++++++++++++++++")
+
+    return grade
+
+
+
+def go_back_menu():
+    number=0
+    print("hello")
